9b093267269963fdffa3a9322e1adffe3580ae6d
lagman <asliddin5006>
2025-02-17 00:14:54 +0500

    ulkan o'zgarish

--- py.py
+++ py.py
@@ -1,7 +1,7 @@
-def func1(prime: int, prime2: int)->int:
-    return prime + prime2
+def func1(prime: list, prime2: int)->list:
+    return [prime3 for prime3 in prime if prime3 < prime2]
 
 
 
-func2=func1(2,3)
+func2=func1([1,2,3],10)
 print(func2)
